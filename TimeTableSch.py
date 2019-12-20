e0f2876b67230531b7345105d9b89e1b4ad44ebd
Mohammad Khatibzadeh <mohammad.khatibzadeh.3@gmail.com>
2019-12-20 11:50:32 +0330

    gret. huristic is done

--- TimeTableSch.py
+++ TimeTableSch.py
@@ -1,6 +1,9 @@
 from collections import Counter
-lissti = [0,0,0,4,4,'11','11',11,'b']
-print(Counter(lissti))
+import math
+# lissti = [0,0,0,4,4,'11','11',11,'b']
+# checker = dict(Counter(lissti))
+# print(type(checker))
+# print(checker)
 
 class Teacher_DB:
     #parameters that nedds to be fill during the proccess
@@ -50,19 +53,41 @@ class Main_DB:
 
 
 class Teacher_Time_Calc:
+    #store the number of each lessons that how many teacher can handle them
+    no_teachers_cteach = {}
     def __init__ (self, db_lessAndTime_obj):
     #def eachlesson_calc(self, db_lessAndTime_obj):
         self.all_list_teachers_and_lessons = db_lessAndTime_obj.get_all_teach_less()
         self.total_lessons_time = db_lessAndTime_obj.get_all_total_time()
         print('all list teacher is {} and the total lesson time is : {}  '.format(self.all_list_teachers_and_lessons, self.total_lessons_time))
-
+    
     def each_lesson_calc(self):
-        
+        #this will save the each lesson time requierd for each teacher
+        each_lesson_time_needed = []
+        #this value will storeall teachers lessons
+        lessons_a = []
+        #this loop will count the number of teachers that can teach each lesson
+        for i in range(len(self.all_list_teachers_and_lessons)):
+            lessons_a.extend(self.all_list_teachers_and_lessons[i][1])
+        #this value will store the dict of the number of teachers that can teach each lesson
+        number_teachers_cteach = dict(Counter(lessons_a))
+        #this loop will predict the what time each lesson takes from the teacher that are trying to teach it
+        for c in range(len(self.total_lessons_time)):
+            search_key = self.total_lessons_time[c][0]
+            search_value = self.total_lessons_time[c][1]
+            if search_key in number_teachers_cteach.keys():
+                devider_um = math.ceil(search_value / number_teachers_cteach[search_key])
+                each_lesson_time_needed.append([search_key, devider_um])
+        print('this is each lesson time needed{}'.format(each_lesson_time_needed))
+                
+                    
+
+
 
 
 b0 = Main_DB()
 b1 = Teacher_DB(b0)
-b3 = Teacher_DB(b0)
-b4 = Teacher_DB(b0)
+b2 = Teacher_DB(b0)
 b2 = Teacher_Time_Calc(b0)
+b2.each_lesson_calc()
 
